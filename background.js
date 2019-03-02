418d4d3bfe0c0b24805102886b817d3b8cd0153d
ramadis <rramiro.o@hotmail.com>
2019-03-01 22:54:31 -0300

    Start testing options to hide or disable extension on not whitelisted sites

--- background.js
+++ background.js
@@ -1,6 +1,16 @@
-// On click icon, download
-chrome.browserAction.onClicked.addListener(function(tab) {
-  const { url } = tab;
+// disables active tab
+const disableActiveTab = () =>
+  chrome.tabs.query({ currentWindow: true, active: true }, ([tab]) =>
+    chrome.browserAction.disable(tab.id)
+  );
+
+const enableActiveTab = () =>
+  chrome.tabs.query({ currentWindow: true, active: true }, ([tab]) =>
+    chrome.browserAction.enable(tab.id)
+  );
+
+// get script's file path for a given url
+const getScriptFromUrl = url => {
   let file = "";
   if (url.includes("espaebook2.com")) file = "options/espaebooks/onclick.js";
   else if (url.includes("bajaebooks2.net"))
@@ -9,6 +19,19 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   else if (url.includes("epublibre.org")) file = "options/epublibre/onclick.js";
   else if (url.includes("lectulandia.com"))
     file = "options/lectulandia/onclick.js";
+  return file;
+};
 
+// enable extensions only in included websites
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, { url }) => {
+  if (!url) return;
+  else if (!getScriptFromUrl(url)) disableActiveTab();
+  else enableActiveTab();
+});
+
+// on click, triggers download script
+chrome.browserAction.onClicked.addListener(tab => {
+  const { url } = tab;
+  const file = getScriptFromUrl(url);
   chrome.tabs.executeScript({ file });
 });
